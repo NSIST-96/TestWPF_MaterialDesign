3c4ef48eb3305c1ffdd7da0d9547b56287bbfaac
NSIST96 <amelin.good@mail.ru>
2023-08-14 14:28:51 +0300

    Добавлена PageModel

--- TestWPF_MaterialDesign/Model/PageModel.cs
+++ TestWPF_MaterialDesign/Model/PageModel.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace TestWPF_MaterialDesign.Model
 {
-    internal class PageModel
+    public class PageModel
     {
+        public int  CustomerCount { get; set; }
+        public string CustomerStatus { get; set; }
+        public DateOnly OrderDate { get; set; }
+        public decimal TransactionValue { get; set; }
+        public TimeOnly ShirmentDelivery { get; set; }
+        public bool LocationStatus { get; set; }
     }
 }
